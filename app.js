f00c539e75dd20e2c4dfb8cfa0decbb93539be60
aubreyworley <aubreyworley@gmail.com>
2015-08-29 17:21:01 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+angular.module('bookspiration', [])
+
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.searchCategories = function () {
+
+      console.log($scope.categories);
+      var url = 'https://www.googleapis.com/books/v1/volumes?q=categories/' + $scope.categories + '&key=AIzaSyBaSFFH24qPxVfKqN1ln_umX33Yfk9CA-g&callback=JSON_CALLBACK'; 
+      // https://www.googleapis.com/books/v1/volumes?q=categories&key=AIzaSyBaSFFH24qPxVfKqN1ln_umX33Yfk9CA-g
+      // var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
+      $http.jsonp(url)
+        .then(function (response) {
+          console.log(response);
+        }); 
+    };
+  }]);
